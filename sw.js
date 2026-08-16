b3030471cf1bdf3f7ccbd8eb311ecd41504692d5
lamartinazzo <lamartinazzo@gmail.com>
2026-08-16 16:45:13 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,34 @@
-const C="glicose-v6";self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.add("./"))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request))));
+const VERSION = "glicose-v7";
+const CACHE = VERSION;
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./sw.js"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys
+          .filter(key => key.startsWith("glicose-") && key !== CACHE)
+          .map(key => caches.delete(key))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
+
+self.addEventListener("message", event => {
+  if (event.data === "SKIP_WAITING") self.skipWaiting();
+});
