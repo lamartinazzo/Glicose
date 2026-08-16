5b936199820152aa952bd32f68a717a0582c64de
lamartinazzo <lamartinazzo@gmail.com>
2026-08-16 18:36:58 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION="glicose-v17",CACHE=VERSION,ASSETS=["./","./index.html","./manifest.webmanifest","./sw.js"];
+const VERSION="glicose-v18",CACHE=VERSION,ASSETS=["./","./index.html","./manifest.webmanifest","./sw.js"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith("glicose-")&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{if(e.request.method==="GET")e.respondWith(fetch(e.request,{cache:"no-store"}).then(r=>{caches.open(CACHE).then(c=>c.put(e.request,r.clone()));return r}).catch(()=>caches.match(e.request)))});
