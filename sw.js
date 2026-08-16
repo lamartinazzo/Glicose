4145d39c1d15ae7fbf2819cf6dfceba6886572b4
lamartinazzo <lamartinazzo@gmail.com>
2026-08-16 18:09:48 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,5 @@
-const VERSION="glicose-v16",CACHE=VERSION,ASSETS=["./","./index.html","./manifest.webmanifest","./sw.js"];
+const VERSION="glicose-v17",CACHE=VERSION,ASSETS=["./","./index.html","./manifest.webmanifest","./sw.js"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith("glicose-")&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener("fetch",e=>{
-  if(e.request.method!=="GET")return;
-  const u=new URL(e.request.url);
-  if(u.pathname.endsWith("/")||u.pathname.endsWith("/index.html")){
-    e.respondWith(fetch(e.request,{cache:"no-store"}).then(r=>{caches.open(CACHE).then(c=>c.put(e.request,r.clone()));return r}).catch(()=>caches.match(e.request)));
-  } else {
-    e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request)));
-  }
-});
+self.addEventListener("fetch",e=>{if(e.request.method==="GET")e.respondWith(fetch(e.request,{cache:"no-store"}).then(r=>{caches.open(CACHE).then(c=>c.put(e.request,r.clone()));return r}).catch(()=>caches.match(e.request)))});
 self.addEventListener("message",e=>{if(e.data==="SKIP_WAITING")self.skipWaiting()});
