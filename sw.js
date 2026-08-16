5efb4c2c641fe0f394602ff746fd4578d3116e0e
lamartinazzo <lamartinazzo@gmail.com>
2026-08-16 16:18:55 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,17 +1,1 @@
-const CACHE = "glicose-v1";
-const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
-
-self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request))
-  );
-});
+const C="glicose-v2";self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.add("./"))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request))));
