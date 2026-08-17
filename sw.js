5f4ee0751a147c540c246f983040d33c0a61f16c
lamartinazzo <lamartinazzo@gmail.com>
2026-08-17 11:18:28 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION="glicose-v19",CACHE=VERSION,ASSETS=["./","./index.html","./manifest.webmanifest","./sw.js","./icon-192.png","./icon-512.png"];
+const VERSION="glicose-v22",CACHE=VERSION,ASSETS=["./","./index.html","./manifest.webmanifest","./sw.js","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith("glicose-")&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;const u=new URL(e.request.url);if(u.pathname.endsWith("/")||u.pathname.endsWith("/index.html"))e.respondWith(fetch(e.request,{cache:"no-store"}).then(r=>{caches.open(CACHE).then(c=>c.put("./index.html",r.clone()));return r}).catch(()=>caches.match("./index.html")));else e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request)))});
